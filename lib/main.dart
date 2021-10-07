793b830b913bb965d96de33eedda5590ee69727a
novaliazahra0023 <91902318+novaliazahra0023@users.noreply.github.com>
2021-10-07 21:58:51 +0700

    melakukan perubahan di lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,15 @@ class Home extends StatelessWidget{
           Icon(Icons.more_vert, color:Colors.black)
         ],
       ),
+    body: Container(
+      color: Colors.blue, 
+      margin: EdgeInsets.symmetric(horizontal:20, vertical:20),
+      padding: EdgeInsets.all(30),
+      child: Icon(
+        Icons.home, 
+        color: Colors.red,
+        size: 100.78),
+    ),
     );
   }
 }
